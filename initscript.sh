e2b2ee078f22b4464062da4d6ae85962d3c98f53
David Francos (XayOn) <opensource@davidfrancos.net>
2012-02-17 14:57:58 +0100

    Made it cleaner. Does same stuff, it's readable now.

--- initscript.sh
+++ initscript.sh
@@ -1,1 +1,48 @@
-mkdir -p /root/dlabs; cd /root/dlabs; rm -rf *; svn co http://trac.aircrack-ng.org/svn/trunk aircrack-ng-1.1 && cd aircrack-ng-1.1 && svn info && { rm -rf `find . -name .svn` && tar czvf  ../aircrack-ng_1.1.orig.tar.gz *; } &>/dev/null && git clone http://github.com/XayOn/Aircrack-ngDebian debian &>/dev/null && vim debian/changelog && debuild $@ && cd .. && dput repo.degeneratedlabs.net *changes && git clone http://github.com/XayOn/Dlive-Airo/ && cd Dlive-Airo && lb build && scp *iso 192.168.1.3:
+#!/bin/bash
+# Copyright 2012 David Francos Cuartero
+# This File is licensed under the GNU General Public License version 2 or superior (GPL2+)
+# Degeneratedlabs initscript
+# Each time you launch the virtual machine it will recreate the debian packages and dlive
+
+cleanup(){ (( $(ls -l|wc -l) > 3 )) && rm -rf /root/*; }
+mkdirs(){ mkdir -p /root/dlabs; cd /root/dlabs; cleanup; }
+
+get_aircrackng(){
+  svn co http://trac.aircrack-ng.org/svn/trunk aircrack-ng-1.1
+  cd aircrack-ng-1.1 
+  rm -rf `find . -name .svn` 
+  tar czvf  ../aircrack-ng_1.1.orig.tar.gz *; 
+}
+
+get_debian(){
+  # Remember to edit debian/changelog each time you build a new version. 
+  # Directly doing this in github editor should be good enough!
+  git clone http://github.com/XayOn/Aircrack-ngDebian debian 
+}
+
+build_package(){ 
+  debuild -us -uc $@
+}
+
+build_live(){
+  git clone http://github.com/XayOn/Dlive-Airo/ 
+  cd Dlive-Airo
+  lb build 
+}
+
+upload_package(){ 
+  cd .. 
+  dput repo.degeneratedlabs.net *changes
+}
+
+upload_live(){
+  scp *iso 192.168.1.3:
+}
+
+cleanup
+get_aircrackng
+get_debian
+build_package
+upload_package
+build_live
+upload_live
