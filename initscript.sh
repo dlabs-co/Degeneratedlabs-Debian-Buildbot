09f8dce43ec6dcb3df028ff09a1de761b755d1f1
David Francos (XayOn) <opensource@davidfrancos.net>
2012-05-02 16:40:15 +0200

    Lots of changes, too lazy to write a proper log

--- initscript.sh
+++ initscript.sh
@@ -4,50 +4,49 @@
 # Degeneratedlabs initscript
 # Each time you launch the virtual machine it will recreate the debian packages and dlive
 
-install_tools(){ apt-get install devscripts dh_make debhelper live-build live-config live-tools; }
+dir=/root/
+logfile=${dir}/`date +%m-%d`.log
 
+install_tools(){ apt-get install devscripts dh-make debhelper live-build live-config live-tools; }
 cleanup(){ (( $(ls -l|wc -l) > 3 )) && rm -rf /root/*; }
-mkdirs(){ mkdir -p /root/dlabs; cd /root/dlabs; cleanup; }
+mkdirs(){ mkdir -p $dir/dlabs; cd $dir/dlabs; cleanup; }
 
 get_aircrackng(){
-  svn co http://trac.aircrack-ng.org/svn/trunk aircrack-ng-1.1
-  cd aircrack-ng-1.1 
-  rm -rf `find . -name .svn` 
-  tar czvf  ../aircrack-ng_1.1.orig.tar.gz *; 
-}
-
-get_debian(){
-  # Remember to edit debian/changelog each time you build a new version. 
-  # Directly doing this in github editor should be good enough!
-  git clone http://github.com/XayOn/Aircrack-ngDebian debian 
-}
-
-build_package(){ 
-  debuild -us -uc $@
+  echo "Getting aircrack-ng from subversion"
+  svn co http://trac.aircrack-ng.org/svn/trunk aircrack-ng-1.1 && {
+      cd aircrack-ng-1.1 
+      rm -rf `find . -name .svn` 
+      echo "Creating orig file"
+      tar czvf  ../aircrack-ng_1.1.orig.tar.gz *; 
+      git clone http://github.com/XayOn/Aircrack-ngDebian debian 
+  }
 }
 
 build_live(){
-  git clone http://github.com/XayOn/Dlive-Airo/ 
-  cd Dlive-Airo
-  lb build 
+    echo "Getting dlive airo from git"
+    git clone http://github.com/Degeneratedlabs/Dlive-Airo/ 
+    cd Dlive-Airo
+    echo "Building live"
+    lb build  &> $logfile
+    scp *iso $remoteuser@192.168.1.3:
+}
+
+build_package(){
+    echo "Downloading aircrack-ng"
+    get_aircrackng &> $logfile
+    echo "Building package"
+    debuild -us -uc $@ >& $logfile
+    echo "Uploading package"
+    upload_package &> $logfile
 }
 
 upload_package(){ 
-  cd .. 
-  dput repo.degeneratedlabs.net *changes
+    wget --no-check-certificate https://raw.github.com/degeneratedlabs/Degeneratedlabs-Debian-Buildbot/master/.dput.cf -O ~/.dput.cf
+    cd .. 
+    dput repo.degeneratedlabs.net *changes
 }
 
-upload_live(){
-  scp *iso 192.168.1.3:
-}
-
-cleanup
-install_tools
-
-get_aircrackng
-get_debian
+mkdirs
+install_tools &> $logfile
 build_package
-upload_package
-
-build_live
-upload_live
+build_live 
